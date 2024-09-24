c29d203511b139eba186e1101c0c13ddfbe1fb40
lucas cordeiro da Silva <lukas_civil@hotmail.com>
2024-09-24 17:37:39 -0300

    Update challenge-3.tsx

--- src/challenges/challenge-3.tsx
+++ src/challenges/challenge-3.tsx
@@ -1,20 +1,32 @@
 import { useCallback, useEffect, useState } from "react";
 
+const commonTheme = {
+  padding: "10px",
+  borderRadius: "8px",
+};
+
 const lightTheme = {
+  ...commonTheme,
   backgroundColor: "white",
   color: "black",
 };
 
 const darkTheme = {
+  ...commonTheme,
   backgroundColor: "black",
   color: "white",
 };
 
-// Build a custom hook to change the theme based on user action.
+// Build a custom hook to change the theme based on user button click action.
+// Use all TypeScript system resources.
 const Challenge3 = () => {
   return (
     <div style={lightTheme}>
       <h1>Challenge 2</h1>
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <button value="light">Light</button>
+        <button value="dark">Dark </button>
+      </div>
     </div>
   );
 };
