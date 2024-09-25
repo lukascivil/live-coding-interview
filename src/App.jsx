2ff78de7d1ded9fcc99da1b493e39be383d682da
lucas cordeiro da Silva <lukas_civil@hotmail.com>
2024-09-24 21:25:34 -0300

    chore: adjust challenge 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 // Challenges
 import Challenge1 from "./challenges/challenge-1";
 import Challenge2 from "./challenges/challenge-2";
 import Challenge3 from "./challenges/challenge-3";
+import Challenge4 from "./challenges/challenge-4";
 
 function App() {
-  return <Challenge3 />;
-
-  // return (
-  //   <>
-  //     <div>
-  //       <a href="https://vitejs.dev" target="_blank">
-  //         <img src={viteLogo} className="logo" alt="Vite logo" />
-  //       </a>
-  //       <a href="https://react.dev" target="_blank">
-  //         <img src={reactLogo} className="logo react" alt="React logo" />
-  //       </a>
-  //     </div>
-  //     <h1>Vite + React</h1>
-  //     <div className="card">
-  //       <button onClick={() => setCount((count) => count + 1)}>
-  //         count is {count}
-  //       </button>
-  //       <p>
-  //         Edit <code>src/App.jsx</code> and save to test HMR
-  //       </p>
-  //     </div>
-  //     <p className="read-the-docs">
-  //       Click on the Vite and React logos to learn more
-  //     </p>
-  //   </>
-  // );
+  return <Challenge4 />;
 }
 
 export default App;
--- src/challenges/challenge-4.tsx
+++ src/challenges/challenge-4.tsx
@@ -1,9 +1,7 @@
-import { useCallback, useEffect, useState } from "react";
-
-const createUserRefund = () => {
+const createUserRefund = (id) => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve({ done: true });
+      resolve({ done: true, id });
     }, 1000);
   });
 };
@@ -16,22 +14,34 @@ const createUserRefundLog = () => {
   });
 };
 
+const getUserExtract = () => {
+  const extract = new Array(1000).fill(0).map((_, index) => ({
+    id: index,
+    value: Math.random() * 100,
+  }));
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(extract);
+    }, 1000);
+  });
+};
+
 /**
  * Challenge 4
  *
- * The user need to send a very important request to the server.
- * (1) Identify opportunities for improvement
+ * The user should see the extract and be able to send the refund request.
+ * The user should never register the refund twice.
+ * (1) List the user extract.
+ * (2) Create button to send the refund and block.
+ * (3) Identify opportunities for improvement (double click).
+ * (4) Tell about the pagination strategy.
+ * (5) Use all TypeScript system resources.
  */
 const Challenge4 = () => {
-  const userClickTwice = useCallback(() => {
-    createUserRefund();
-    createUserRefundLog();
-  }, []);
-
   return (
     <div>
       <h1>Challenge 4</h1>
-      <button onClick={userClickTwice}>Send user refund</button>
     </div>
   );
 };
